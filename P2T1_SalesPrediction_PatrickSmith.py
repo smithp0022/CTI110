855e45be40965357871b173c5bd3fcfbcc85a5e4
smithp0022 <47290119+smithp0022@users.noreply.github.com>
2019-02-03 01:40:44 -0600

    Add files via upload
    First submission.

---
+++ P2T1_SalesPrediction_PatrickSmith.py
@@ -0,0 +1,16 @@
+
+# Calculate the projected annual profit for Company A
+# 02 FEB 2019
+# CTI-110 P2T1 - Sales Prediction
+# Patrick Smith
+
+# input the total sales
+total_sales = float(input("Enter the projected sales: "))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print("the profit is $", format(profit, ",.2f"))
+
+input("Press the 'Enter Key' to exit")
