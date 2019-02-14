0000d0ea89bb4cda81340069492b2f45f22d38e1
smithp0022 <47290119+smithp0022@users.noreply.github.com>
2019-02-14 09:46:23 -0500

    Add files via upload

---
+++ P2HW1_PoundsKilograms_PatrickSmith.py
@@ -0,0 +1,16 @@
+
+# Convert pounds to kilograms
+# 02 FEB 2019
+# CTI-110 P2HW1 - Pounds to Kilograms converter
+# Patrick Smith
+
+# input the pounds (lbs) to be converted to kilograms (kg)
+lb = float(input("Enter the pounds to be converted: "))
+
+# Calculate the coversion using kg=lb/2.2046.
+kg = lb/2.2046
+
+# Display the kilograms.
+print("\nkg is", format(kg, ",.2f"))
+
+input("\nPress the 'Enter Key' to exit")
